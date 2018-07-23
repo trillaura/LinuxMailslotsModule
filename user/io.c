aeeb59d896fda554ad087964289e7f738ce481e4
trillaura <laura.trivelloni@gmail.com>
2018-07-23 12:21:01 +0200

    added reader/writer/controller

--- user/io.c
+++ user/io.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <pthread.h> 
@@ -7,37 +8,63 @@
 #include <sys/sysmacros.h>
 #include <string.h>
 #include <sys/wait.h>
-
-char *device = "/dev/mailslot1";
+#include <sys/ioctl.h>
 
 void *writer(void *arg) {
-	int w = open(device, O_WRONLY);
-	char *hello = "hello";
-	write(w, hello, strlen(hello));
+	int fd = open((char * )arg, O_WRONLY);
+	char *text[6] = {"Sempre","caro","mi", "fu", "quest'ermo", "colle"};		
+	int i;
+	int size = sizeof(text)/sizeof(char *);
+	for (i = 0; i < size; i++) {
+		printf("Writing %s\n", *(text+i));
+		write(fd, *(text+i), strlen(*(text+i)));
+	} 
 }
 
 void *reader(void *arg) {
-	int r = open(device, O_RDONLY);
+	int fd = open((char *)arg, O_RDONLY);
         char buff[256];
-        read(r, buff, 256);
-        printf("Read %s\n", buff);	
+	int i=0;
+        while (read(fd, buff, 256) != 0 && i<5) {
+        	printf("%s\n", buff);
+		i++;
+		memset(&buff[0], 0, sizeof(buff));
+	}	
 }
 
-int main() {
+void *controller(void *arg) {
+	int fd = open((char*) arg, O_RDWR);
+	ioctl(fd, 0, 0); 
+}
 
-	mknod(device, 0666 | S_IFCHR, makedev(242, 1));
+int main(int argc, char **argv) {
+
+	if (argc != 4) {
+		printf("Usage <device> <major> <minor>\n");
+		return -1;
+	}	
+	
+	char *prefix = "/dev/";
+	char *name = (char *) malloc(strlen(prefix) + strlen(argv[1]) + 1);
+	strcpy(name, prefix);
+	strcat(name, argv[1]);
+	int major = atoi(argv[2]);
+	int minor = atoi(argv[3]);
+
+	mknod(name, 0666 | S_IFCHR, makedev(major, minor));
 	
 	int status = 0;
 
-	writer(NULL);
 	pid_t child;
+	//int w = open(name, O_RDWR);
 	if ((child = fork()) == 0) {
-		printf("oo\n");
-		writer(NULL);
+		writer((void *) name);
 		return 0;
 	}
 	waitpid(child, &status, 0);
-	printf("Written\n");
-	reader(NULL);
+	//printf("Written\n");
+	reader((void *) name);
+
+	controller((void*)name);
 	return 0;
 }
