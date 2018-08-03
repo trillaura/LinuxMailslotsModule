8fce32f5799c46fe9140927fefa1f97b4e02df52
trillaura <laura.trivelloni@gmail.com>
2018-08-03 10:49:46 +0200

    clean files

--- module/.mailslots.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/trillaura/Desktop/Università/PSMSM/Project/module/mailslots.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/trillaura/Desktop/Università/PSMSM/Project/module/mailslots.ko /home/trillaura/Desktop/Università/PSMSM/Project/module/mailslots.o /home/trillaura/Desktop/Università/PSMSM/Project/module/mailslots.mod.o ;  true
--- module/.tmp_versions/mailslots.mod
+++
@@ -1,3 +0,0 @@
-/home/trillaura/Desktop/Università/PSMSM/Project/module/mailslots.ko
-/home/trillaura/Desktop/Università/PSMSM/Project/module/mailslots.o
-
